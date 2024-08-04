c1b1cc588f634ad5f399a92eaed343ac85d799d4
selimnill <mrrafi812345@gmail.com>
2024-08-04 21:50:44 +0600

    Header and footer added

--- src/Routes/MainRoute.jsx
+++ src/Routes/MainRoute.jsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layouts/MainLayout";
+import Home from "../Pages/Home";
 
 
 export const router = createBrowserRouter ([
     {
         path: '/',
-        element: <MainLayout />
+        element: <MainLayout />,
+        children: [
+            {
+                path: '/',
+                element: <Home />
+            }
+        ]
     }
 ])
